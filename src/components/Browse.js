353f478328240823c4011c251daeec7bdb3a1976
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-01-18 04:14:11 +0530

    updated code

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,21 +1,8 @@
-import { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
 import Header from "./Header";
+import userNowPlayingMovies from "../hooks/useNowPlayingMovies";
 
 const Browse = () => {
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json);
-  };
-
-  //i make my api call inside a use Effect - so that i can call it only ones.[] dont forget (if dont use - infinite api calls)
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
+  userNowPlayingMovies();
   return (
     <div>
       <Header />
