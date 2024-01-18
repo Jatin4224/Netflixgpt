ffde563babbcc5cfdee34943af18665ff6a7b47d
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-01-18 18:54:26 +0530

    code updated

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,11 +1,15 @@
 import Header from "./Header";
 import userNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
   userNowPlayingMovies();
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
       {/* 
          MainContainer =
          - videoBackgorund
