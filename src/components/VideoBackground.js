e2a469caca264660be0d8ef72169d1b99348f359
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-01-19 01:08:35 +0530

    added trailer

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,17 +1,34 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideo } from "../utils/moviesSlice";
 
 const VideoBackground = ({ movieId }) => {
   const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+  //fetch trailer video
+  const dispatch = useDispatch();
+  const getMovieVideos = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/976573/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    console.log(json);
 
+    //mvie has two trailers - need onw only
+    const filterData = json.results.filter((video) => video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+    console.log(trailer);
+    dispatch(addTrailerVideo(trailer));
+  };
+
+  useEffect(() => {
+    getMovieVideos();
+  }, []);
   return (
-    <div className=" w-screen">
+    <div>
       <iframe
-        className="w-screen aspect-video"
-        src={
-          "https://www.youtube.com/embed/" +
-          trailerVideo?.key +
-          "?&autoplay=1&mute=1"
-        }
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
--- src/utils/moviesSlice.js
+++ src/utils/moviesSlice.js
@@ -7,9 +7,12 @@ const moviesSlice = createSlice({
     addNowPlayingMovies: (state, action) => {
       state.addNowPlayingMovies = action.payload;
     },
+    addTrailerVideo: (state, action) => {
+      state.trailerVideo = action.payload;
+    },
   },
 });
 
-export const { addNowPlayingMovies } = moviesSlice.actions;
+export const { addNowPlayingMovies, addTrailerVideo } = moviesSlice.actions;
 
 export default moviesSlice.reducer;
